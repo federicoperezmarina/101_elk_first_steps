ad9914818f0c4a52d142db4ea3e22d24f836f696
federicoperezmarina <federicoperezmarina@gmail.com>
2022-08-14 23:56:03 +0200

    feat: adding delete document in index

---
+++ elastic/elastic_delete_document.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch('http://localhost:9200')
+
+resp = es.delete(index="starwars-characters", id=1)
+
+print(resp['result'])
